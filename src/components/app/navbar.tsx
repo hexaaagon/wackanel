d64124795913a2dc5d0724e8a40a81c3b7bba206
Hexaa <me@hexagonn.my.id>
2025-06-18 15:52:48 +0700

    feat(navbar): improve layout and logo size in Navbar component

--- src/components/app/navbar.tsx
+++ src/components/app/navbar.tsx
@@ -5,23 +5,28 @@ import { buttonVariants } from "@/components/ui/button";
 export function Navbar() {
   return (
     <nav className="border-border bg-secondary-background fixed top-0 left-0 z-20 mx-auto flex h-[70px] w-full items-center border-b-4 px-5">
-      <div className="text-foreground mx-auto flex w-[1300px] max-w-full items-center justify-between">
-        <div className="flex items-center gap-10 xl:gap-10">
-          <Link href={"/"}>
+      <div className="text-foreground mx-auto flex w-[1300px] max-w-full items-center text-center">
+        {/* Left section */}
+        <div className="flex flex-1 items-center justify-start">
+          <Link href={"/"} className="flex items-center gap-1 font-semibold">
             <Image
               src="/images/wackanel.svg"
-              height={32}
-              width={32}
+              height={42}
+              width={42}
               alt="Wackanel Logo"
             />
+            <p>Wackanel</p>
           </Link>
-
-          <div className="font-base hidden items-center gap-10 text-base lg:flex xl:gap-10">
-            <Link href="/dashboard">Dashboard</Link>
-            <Link href="/docs">Docs</Link>
-          </div>
         </div>
-        <div className="flex items-center justify-end gap-4">
+
+        {/* Center section */}
+        <div className="font-base hidden flex-1 items-center justify-center gap-5 text-base md:flex">
+          <Link href="/dashboard">Dashboard</Link>
+          <Link href="/docs">Docs</Link>
+        </div>
+
+        {/* Right section */}
+        <div className="flex flex-1 items-center justify-end gap-4">
           <Link href="/auth/login" className={buttonVariants()}>
             Login
           </Link>
