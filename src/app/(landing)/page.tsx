c369077c08d96cad8717cd719337b6d66b431113
Hexaa <me@hexagonn.my.id>
2025-07-02 15:22:39 +0700

    fix(build): added suspense

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,9 +1,12 @@
+import { Suspense } from "react";
 import Hero from "./hero";
 
 export default function Home() {
   return (
     <main className="text-foreground font-base">
-      <Hero />
+      <Suspense fallback={<div className="min-h-[100lvh] flex items-center justify-center">Loading...</div>}>
+        <Hero />
+      </Suspense>
     </main>
   );
 }
---
+++ src/app/dashboard/setup/page.tsx
@@ -0,0 +1,10 @@
+export default async function SetupPage() {
+  return (
+    <main className="flex min-h-[100lvh] items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-2xl font-semibold">Setup Page</h1>
+        <p className="mt-2">coming soom bro, mb</p>
+      </div>
+    </main>
+  );
+}
